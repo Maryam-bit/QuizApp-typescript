adbd319063be009569c7ea3ff2dd822075d92154
Maryam Noor <rajputmaryam5555@gmail.com>
2021-06-04 21:58:27 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/register/register.page'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid app">
+      <Home />
+      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='svg'>
+        <path fill="#00cba9" fill-opacity="1" d="M0,192L80,202.7C160,213,320,235,480,208C640,181,800,107,960,64C1120,21,1280,11,1360,5.3L1440,0L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+      </svg> */}
     </div>
   );
 }
---
+++ src/pages/register/register.page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './register.page.scss'
+import img from '../../img/register.png'
+const Home = () => {
+    return (
+    
+        <div className="registerd d-flex h-100">
+            
+            <div className="card shadow mx-auto my-auto row justify-content-center align-self-center">
+            <div className="register-img text-center">
+            <img src={img} />
+        </div>
+            </div>
+        </div>
+        
+    
+    )
+}
+
+export default Home;
