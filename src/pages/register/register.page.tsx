ab4a7562fdf97474f14ee2741b90c36573d79418
Maryam Noor <rajputmaryam5555@gmail.com>
2021-06-04 23:03:53 +0500

    working on login page

--- src/pages/register/register.page.tsx
+++ src/pages/register/register.page.tsx
@@ -3,17 +3,19 @@ import './register.page.scss'
 import img from '../../img/register.png'
 const Home = () => {
     return (
-    
-        <div className="registerd d-flex h-100">
-            
-            <div className="card shadow mx-auto my-auto row justify-content-center align-self-center">
-            <div className="register-img text-center">
-            <img src={img} />
-        </div>
+
+        <div className="register h-100">
+            <div className=" register-img text-center">
+                <img src={img} />
             </div>
+
+            <div className="card mx-auto my-auto row justify-content-center align-self-center">
+             <h1>h3llo</h1>
+            </div>
+
         </div>
-        
-    
+
+
     )
 }
 
