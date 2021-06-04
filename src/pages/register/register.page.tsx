7ddce209492fbb2e3b510b0a7b3603a81ac11348
Maryam Noor <rajputmaryam5555@gmail.com>
2021-06-04 23:41:13 +0500

    almost regiter page completed

--- src/pages/register/register.page.tsx
+++ src/pages/register/register.page.tsx
@@ -4,13 +4,30 @@ import img from '../../img/register.png'
 const Home = () => {
     return (
 
-        <div className="register h-100">
+        <div className="register h-100 text-center">
             <div className=" register-img text-center">
                 <img src={img} />
             </div>
 
-            <div className="card mx-auto my-auto row justify-content-center align-self-center">
-             <h1>h3llo</h1>
+            <div className="card mx-auto my-auto text-center pt-5">   {/* row justify-content-center align-self-center  */}
+              <br/><br/>
+              <br/><br/>
+            <input type="text" placeholder="Name"
+              /><br/><br/>
+
+              <input type="text" placeholder="Email"
+              /><br/><br/>
+
+              <input type="number" placeholder="Number"
+              /><br/><br/>
+
+              <input type="password" placeholder="Create password"
+              /><br/><br/>
+
+              <button className="login">sign up</button>
+              
+              <p>Signed In?</p>
+
             </div>
 
         </div>
