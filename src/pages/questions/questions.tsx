1f11fcad1483ab99a67c42d3c822e0581c022131
Maryam Noor <rajputmaryam5555@gmail.com>
2021-06-07 00:03:35 +0500

    question complete

--- src/pages/questions/questions.tsx
+++ src/pages/questions/questions.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './questions.styles.scss'
 import Button from "../../components/button/button.component"
+import question from "../../img/question.png"
 const Questions = () => {
     return (
         <div className='questions-container p-4 text-center'>
+            <div className="signup-img text-center">
+          <img src={question} />
+        </div>
             <div className="question">
-                <h2>1. Which is the most beatiful city of the Pakistan?</h2>
+                <h2>Which is the most beatiful city of the Pakistan?</h2>
             </div>
             <form>
                 <label>
