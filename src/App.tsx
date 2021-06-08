3eabce8ba6ecb1e0e36cbc08893c88ac1ebb102e
Maryam Noor <rajputmaryam5555@gmail.com>
2021-06-08 21:45:36 +0500

    navigation bar completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Signup from './pages/signup/signup'
+import Signup from './pages/signup/signup'
 import Signin from './pages/signin/signin'
 import QuizCategory from './pages/quizCategory/quizCategory'
 import Questions from './pages/questions/questions'
 import Result from './pages/result/result'
 import Profile from './pages/profile/profile'
-import SimpleBottomNavigation from './components/tab/tab'
+import Tab from './components/tab/tab'
 import { Route, route, Switch, Redirect } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+
 
 function App() {
+  const screenConfig = React.useState(0);
+  // let a = window.location.pathname
+  // console.log(a)
+  const location = useLocation();
+let a = location.pathname
+
   return (
     <div className="container-fluid app">
       <div className="app-child h-100 d-flex ">
 
       <div className="card mx-auto my-auto">
         <Switch>
-          {/* <Route exact path="/" component={Signup} /> */}
-          <Route exact path="/" component={Profile} />
+          <Route exact path="/" component={Signup} />
+          <Route exact path="/profile" component={Profile} />
           <Route path="/signin" component={Signin} />
           <Route path="/category" component={QuizCategory} />
           <Route path="/questions" component={Questions} />
@@ -27,7 +35,8 @@ function App() {
       
       </div>
       {/* <SimpleBottomNavigation /> */}
-      <SimpleBottomNavigation />
+      {a=='/'? null : a=='/signin' ? null : <Tab/>}
+      {/* <Tab /> */}
     </div>
     {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='svg'>
         <path fill="#00cba9" fill-opacity="1" d="M0,192L80,202.7C160,213,320,235,480,208C640,181,800,107,960,64C1120,21,1280,11,1360,5.3L1440,0L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
--- src/components/tab/tab.tsx
+++ src/components/tab/tab.tsx
@@ -1,34 +1,16 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import React from 'react'
+import {Link} from 'react-router-dom'
 import './tab.styles.scss'
-
-const useStyles = makeStyles({
-  root: {
-    width: 500,
-  },
-});
-
-export default function SimpleBottomNavigation() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
+const Tab = () => {
   return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.root}
-    >
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-    </BottomNavigation>
-  );
+    <div className="tab">
+      <div className="cards">
+        <Link to="/category"><div className="div div1">Dashboard</div></Link>
+        <Link to="/profile"><div className="div div2"> Profile</div></Link >
+        {/* <Link to="/graph"><div className="div">Graph</div></Link> */}
+      </div>
+    </div>
+  )
 }
+
+export default Tab;
